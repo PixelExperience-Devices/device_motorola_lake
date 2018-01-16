5b6417d0f802183f55a509d992e0ea1a7cf5de01
Oleg Matcovschi <omatcovschi@google.com>
2018-01-16 13:41:15 -0800

    lake: gpt-utils: Align partition swap list
    Modify partition swap list to reflect only existing partitions.
    
    Bug: 70214866
    
    Change-Id: I2efe784efa97e3f2960d990a969fc729022a5ceb

--- gpt-utils/gpt-utils.h
+++ gpt-utils/gpt-utils.h
@@ -79,8 +79,12 @@ extern "C" {
 #define AB_SLOT_A_SUFFIX                "_a"
 #define AB_SLOT_B_SUFFIX                "_b"
 #define PTN_XBL                         "xbl"
-#define PTN_SWAP_LIST                   PTN_XBL, "rpm", "tz", "hyp", "storsec", "devcfg", "keymaster", "cmnlib", "cmnlib64", "prov", "pmic", "abl", "fsg", "vbmeta"
-#define AB_PTN_LIST PTN_SWAP_LIST, "modem", "dsp", "bluetooth", "logo", "boot", "system", "oem", "vendor", "dto"
+#define PTN_SWAP_LIST                   PTN_XBL, \
+            "abl", "cmnlib", "cmnlib64", "devcfg", \
+            "dtbo", "fsg", "hyp", "keymaster", "pmic", \
+            "prov", "rpm", "storsec", "tz", "vbmeta"
+
+#define AB_PTN_LIST PTN_SWAP_LIST, "bluetooth", "boot", "dsp", "logo", "modem", "oem", "system", "vendor"
 #define BOOT_DEV_DIR    "/dev/block/bootdevice/by-name"
 
 /******************************************************************************
