f9b0b9dcc03e7df1b0bb58702f1dfbd289822656
Dan Willemsen <dwillemsen@google.com>
2018-04-04 16:49:07 -0700

    lake: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
    Bug: 77599627
    Test: lunch  [see entries in list]
    Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633
    Signed-off-by: Joshua Blanchard <joshua.lee.bbg@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 The LineageOS Open Source Project
+# Copyright (C) 2017-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,3 +16,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_lake.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_lake-eng \
+    lineage_lake-user \
+    lineage_lake-userdebug
