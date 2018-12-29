3f643e79de5e8d99f4136687b7f3a1a3138123f7
rahulsnair <rahulsnair30@gmail.com>
2018-12-29 08:10:28 +0000

    evert: tag vendor into audio calibration file props

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -4,13 +4,13 @@
 
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.audio.calfile0=/vendor/etc/acdbdata/common/Bluetooth_cal.acdb \
-    persist.audio.calfile1=/vendor/etc/acdbdata/common/General_cal.acdb \
-    persist.audio.calfile2=/vendor/etc/acdbdata/common/Global_cal.acdb \
-    persist.audio.calfile3=/vendor/etc/acdbdata/common/Handset_cal.acdb \
-    persist.audio.calfile4=/vendor/etc/acdbdata/common/Hdmi_cal.acdb \
-    persist.audio.calfile5=/vendor/etc/acdbdata/common/Headset_cal.acdb \
-    persist.audio.calfile6=/vendor/etc/acdbdata/common/Speaker_cal.acdb
+    persist.vendor.audio.calfile0=/vendor/etc/acdbdata/common/Bluetooth_cal.acdb \
+    persist.vendor.audio.calfile1=/vendor/etc/acdbdata/common/General_cal.acdb \
+    persist.vendor.audio.calfile2=/vendor/etc/acdbdata/common/Global_cal.acdb \
+    persist.vendor.audio.calfile3=/vendor/etc/acdbdata/common/Handset_cal.acdb \
+    persist.vendor.audio.calfile4=/vendor/etc/acdbdata/common/Hdmi_cal.acdb \
+    persist.vendor.audio.calfile5=/vendor/etc/acdbdata/common/Headset_cal.acdb \
+    persist.vendor.audio.calfile6=/vendor/etc/acdbdata/common/Speaker_cal.acdb
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
