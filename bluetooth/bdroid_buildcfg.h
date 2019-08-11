8a5b110a97d2d7359204a914c7998c1262c95f9a
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2019-08-11 17:18:02 -0400

    lake: change bluetooth device name
    * to Moto G7 Plus
    
    Change-Id: I6f8a1742316677c651fc7300b41bef5659f6dea5
    Signed-off-by: Joshua Blanchard <joshua.lee.bbg@gmail.com>

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -23,7 +23,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME "Moto G6 Plus"
+#define BTM_DEF_LOCAL_NAME "Moto G7 Plus"
 
 #define BLE_VND_INCLUDED   TRUE
 #define BTIF_HF_WBS_PREFERRED TRUE
