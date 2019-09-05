119c97369e0ae1f27c04f3829bf42d0f0a95ff56
Erfan Abdi <erfangplus@gmail.com>
2019-09-05 21:14:08 +0430

    lake: Add uhid group for fingerprint service
    Change-Id: I318d4650a6618c10674a34691bc1b8e43af462b8
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>
    Signed-off-by: Joshua Blanchard <joshua.lee.bbg@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -32,3 +32,7 @@ export DEVICE_BRINGUP_YEAR=2019
 ./../../$VENDOR/$DEVICE_COMMON/extract-files.sh $@
 
 BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
+
+# Add uhid group for fingerprint service
+FP_SERVICE_RC="$BLOB_ROOT"/vendor/etc/init/android.hardware.biometrics.fingerprint@2.1-service-ets.rc
+sed -i "s/input/uhid input/" "$FP_SERVICE_RC"
