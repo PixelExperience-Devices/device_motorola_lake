84c2c4e894cc26c15b610e539b18d6de01a2eb39
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2019-09-09 19:34:01 -0400

    lake: label init.oem.panel.sh and allow execution
    * Needed to load the correct kernel module for the touch panel,
      Lake ships with several variants.
    
    Change-Id: I0f9274a700641dc82c56eb99a37b73d76099d4b8
    Signed-off-by: Joshua Blanchard <joshua.lee.bbg@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,5 +51,8 @@ TARGET_HAS_NO_WLAN_STATS := true
 # RIL
 ENABLE_VENDOR_RIL_SERVICE := false
 
+# SELinux
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+
 # inherit from the proprietary version
 -include vendor/motorola/lake/BoardConfigVendor.mk
