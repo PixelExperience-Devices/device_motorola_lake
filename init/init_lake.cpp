8e0e358b1d72aeb16c193870bb1fd13c0d72088b
Nolen Johnson <johnsonnolen@gmail.com>
2020-05-03 16:19:27 -0400

    lake: Import custom init
    This is a squash of the following commits:
    
      lake: Kang m8's init to set variant properties
    
        * Based loosely on m8's as of c15efa1ffa8e67469dedc14c779030d3405c0821.
    
        * Use SKU as opposed to MID as motorola uses these.
    
        * REVVLRY+ is a lake rebrand for T-Mobile, so we need to set T-Mobile
          IMS properties.
    
        * Let GMS Client ID be set in makefile in common tree.
    
        * Let all SKU's other than "XT1965-T" fall through to lake's unlocked
           model.
    
      Change-Id: I8bb694340ab02f0bcfb95a09337e14c8fd9017b6
    
      lake: Correctly define libinit location
    
        * Adresses: error: 'system/core/init/Android.bp:95:1: "libinit" depends on undefined module "libinit_msm8953"'
    
        Change-Id: Ib663a0dea66abc4c4266989345d057a6237f99fd
    
    Change-Id: I8bb694340ab02f0bcfb95a09337e14c8fd9017b6

---
+++ init/init_lake.cpp
@@ -0,0 +1,108 @@
+/*
+   Copyright (c) 2013, The Linux Foundation. All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are
+   met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+    * Neither the name of The Linux Foundation nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <vector>
+
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
+
+#include <android-base/properties.h>
+#include <android-base/logging.h>
+
+#include "property_service.h"
+#include "vendor_init.h"
+
+using android::base::GetProperty;
+using android::init::property_set;
+
+std::vector<std::string> ro_props_default_source_order = {
+    "",
+    "odm.",
+    "product.",
+    "system.",
+    "vendor.",
+};
+
+void property_override(char const prop[], char const value[], bool add = true)
+{
+    prop_info *pi;
+
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else if (add)
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
+
+void vendor_load_properties()
+{
+    std::string bootsku;
+    std::string device;
+
+    const auto set_ro_build_prop = [](const std::string &source,
+            const std::string &prop, const std::string &value) {
+        auto prop_name = "ro." + source + "build." + prop;
+        property_override(prop_name.c_str(), value.c_str(), false);
+    };
+
+    const auto set_ro_product_prop = [](const std::string &source,
+            const std::string &prop, const std::string &value) {
+        auto prop_name = "ro.product." + source + prop;
+        property_override(prop_name.c_str(), value.c_str(), false);
+    };
+
+    bootsku = GetProperty("ro.boot.hardware.sku", "");
+    if (bootsku == "XT1965-T") {
+        /* T-Mobile REVVLRY+ */
+        property_override("ro.build.description", "lake_revvl_n-user 9 PCWS29.83-56-12 ec5de release-keys");
+        property_override("persist.vendor.radio.customer_mbns", "tmo_usa_ims_default.mbn");
+        property_override("persist.vendor.radio.data_con_rprt", "1");
+        property_override("persist.vendor.ims.playout_delay", "10");
+        property_override("persist.vendor.ims.cam_sensor_delay", "20");
+        property_override("persist.vendor.ims.display_delay", "40");
+        for (const auto &source : ro_props_default_source_order) {
+            set_ro_build_prop(source, "fingerprint", "motorola/lake_revvl/lake:9/PCWS29.83-56-12/ec5de:user/release-keys");
+            set_ro_product_prop(source, "device", "lake_n");
+            set_ro_product_prop(source, "model", "REVVLRY+");
+            set_ro_product_prop(source, "name", "lake_revvl_n");
+        }
+    } else {
+        /* moto g(7) plus (Unlocked) */
+        property_override("ro.build.description", "lake-user 10 QPW30.61-21 d18ed release-keys");
+        for (const auto &source : ro_props_default_source_order) {
+            set_ro_build_prop(source, "fingerprint", "motorola/lake_retail/lake:10/QPW30.61-18/d18ed:user/release-keys");
+            set_ro_product_prop(source, "device", "lake");
+            set_ro_product_prop(source, "model", "moto g(7) plus");
+        }
+    }
+
+    device = GetProperty("ro.product.device", "");
+    LOG(ERROR) << "Found bootsku '" << bootsku.c_str() << "' setting build properties for '" << device.c_str() << "' device\n";
+}
