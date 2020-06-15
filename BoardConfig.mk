9b11f473257442b5632e1fa7a6ca90f67081f429
Nolen Johnson <johnsonnolen@gmail.com>
2020-06-15 09:13:07 -0400

    lake: Commonize bluetooth configuration
    Change-Id: I708a0f3e59d58958b43edc16a8c9c34a62efbe80

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,9 +24,6 @@ DEVICE_PATH := device/motorola/lake
 # Assertions
 TARGET_OTA_ASSERT_DEVICE := lake
 
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
-
 # Display
 TARGET_SCREEN_DENSITY := 420
 
