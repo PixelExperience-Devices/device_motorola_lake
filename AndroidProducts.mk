5e6d9d0926e117366fbb7e425cacccce2142274e
Jorge Lucas <jorgelucas@pixelexperience.org>
2020-06-21 15:32:01 +0200

    lake: Pixel Experience Bringup
    Signed-off-by: Jorge Lucas <jorgelucas@pixelexperience.org>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lake.mk
+    $(LOCAL_DIR)/aosp_lake.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lake-eng \
-    lineage_lake-user \
-    lineage_lake-userdebug
+    aosp_lake-eng \
+    aosp_lake-user \
+    aosp_lake-userdebug
