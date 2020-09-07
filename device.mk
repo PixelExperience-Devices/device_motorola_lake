df52d2417ef17525d0c38ec9ff9b3a72456d7965
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-09-07 07:02:53 -0300

    lake: Track and build MotCamera2

--- device.mk
+++ device.mk
@@ -69,6 +69,10 @@ PRODUCT_BOOT_JARS += qcom.fmradio
 PRODUCT_PACKAGES += \
     init.lake.rc
 
+# MotCamera2
+PRODUCT_PACKAGES += \
+    MotCamera2
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.1-service \
