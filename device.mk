b437a64af0c1a9f1b4ad1f6247934d363c5a90c0
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2020-09-23 00:03:55 -0400

    lake: Switch to prebuilt audio hal
    Change-Id: I60d5e10c9f52a420af86b249d2eed151b5d88ae0

--- device.mk
+++ device.mk
@@ -38,6 +38,8 @@ PRODUCT_AAPT_PREF_CONFIG := 560dpi
 PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
 
 # Audio
+PREBUILT_AUDIOHAL := true
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
