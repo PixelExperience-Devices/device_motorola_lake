236d6883678a980f0cf439bdc054040f71faecec
Erfan Abdi <erfangplus@gmail.com>
2020-12-30 23:38:25 +0330

    lake: Fix camera recording
    * Patched libmmcamera2_pproc_modules to not look for manufacturer prop, when
      it finds motorola as manufacturer camera hal will start some processes that
      leads to crash on android R...
    * I'm not sure whats downside of this hexpatch but camera recording is fixed...
    
    Change-Id: I8f67d0b07063df30af325a6b8d4181c90c18202f

--- extract-files.sh
+++ extract-files.sh
@@ -12,7 +12,13 @@ function blob_fixup() {
         vendor/etc/init/android.hardware.biometrics.fingerprint@2.1-service-ets.rc)
             sed -i "s/input/uhid input/" "${2}"
             ;;
-    esac
+
+        # Fix camera recording
+        vendor/lib/libmmcamera2_pproc_modules.so)
+            sed -i "s/ro.product.manufacturer/ro.product.nopefacturer/" "${2}"
+            ;;
+
+   esac
 }
 
 # If we're being sourced by the common script that we called,
