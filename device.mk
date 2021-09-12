f46a0180410d09019176a267bc740d2577b0c28a
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-12 19:09:53 +0200

    lake: Remove PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE
    * Deprecated
    
    Change-Id: I1b3638b07ed45f6f72b551d1d241d48f661310c6

--- device.mk
+++ device.mk
@@ -21,7 +21,6 @@ $(call inherit-product, vendor/motorola/lake/lake-vendor.mk)
 -include $(LOCAL_PATH)/vendor_prop.mk
 
 PRODUCT_ACTIONABLE_COMPATIBLE_PROPERTY_DISABLE := true
-PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := false
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
