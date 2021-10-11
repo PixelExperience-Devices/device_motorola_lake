151816853a1f191cdc3a9bb24cf2f67f50b189d0
Jeferson <jroliveira.oliveira301@gmail.com>
2021-10-11 16:00:10 +0200

    lake: Don't build Moto Camera
    Change-Id: Ic4c49f686753d6965c98cb1c1a71a06d047b5fdd

--- device.mk
+++ device.mk
@@ -57,10 +57,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.lake.rc
 
-# MotCamera2
-PRODUCT_PACKAGES += \
-    MotCamera2
-
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service \
