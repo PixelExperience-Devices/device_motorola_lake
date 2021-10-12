a045639162a54cee805389449709c2467a83aa3d
Jorge Lucas <jorgelucas.sd@gmail.com>
2021-10-12 12:54:26 -0300

    lake: Disable some google apps to save space
    Change-Id: Ia3253998ebf595609f4d332b2fad4128d4b10d1d

--- aosp_lake.mk
+++ aosp_lake.mk
@@ -28,6 +28,7 @@ $(call inherit-product, device/motorola/lake/device.mk)
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
 
 # A/B updater
 AB_OTA_POSTINSTALL_CONFIG += \
